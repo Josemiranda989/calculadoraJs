7f3bb31d6ec14cbad27a29846138cbaae33962c2
José Miranda <josemiranda989@gmail.com>
2022-01-04 16:57:06 -0300

    first commit

---
+++ javascript/calculadora.js
@@ -0,0 +1,47 @@
+window.addEventListener('load', function () { //Escuchamos cuando se carga el documento
+
+    // Creamos dos constantes y nos guardamos los elementos que necesitamos
+    const display = document.querySelector('.calculator-display');
+    const keypadButtons = document.getElementsByClassName('keypad-button');
+
+    // Creamos otra constante para convertir el HTMLCollection a Array
+    const keytpadButtonsArray = Array.from(keypadButtons);
+
+    // Itearamos por nuestro nuevo array de botones
+    keytpadButtonsArray.forEach(function (button) {
+
+        // A cada boton le agregamos un listener
+        button.addEventListener('click', () => {
+            calculadora(button, display);
+        })
+    })
+})
+
+function calculadora(button, display) {
+    switch (button.innerHTML) {
+        case "C":
+            borrar(display)
+            break;
+        case "=":
+            calcular(display)
+            break;
+        default:
+            actualizar(display, button)
+            break;
+    }
+}
+
+function calcular(display) {
+display.innerHTML = eval(display.innerHTML) /* Toma el string, lo resuelve y lo guarda en el innerHTML del display */
+}
+
+function actualizar(display, button) {
+    if (display.innerHTML == 0) {
+        display.innerHTML = "";
+    }
+    display.innerHTML += button.innerHTML;
+}
+
+function borrar(display) {
+    display.innerHTML = 0
+}
